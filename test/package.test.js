e15f87e2fdfd6db0124595775ee9f4458eb06a02
Jared Hanson <jaredhanson@gmail.com>
2016-01-06 19:46:04 -0800

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('passport-oauth2-userinfo', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
